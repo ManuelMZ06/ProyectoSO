ecdaf3eae178eceadf6f2f4e17dbff7dc18a7923
Manuel Maquin <manuelzuniga005@gmail.com>
2024-10-12 15:12:26 -0600

    :rocket: Agregando SubMenús

---
+++ AlgoritmosDePlanificacion/SubMenuExpropiativos.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AlgoritmosDePlanificacion
+{
+    public partial class SubMenuExpropiativos : Form
+    {
+        public SubMenuExpropiativos()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRegresarMenu1_Click(object sender, EventArgs e)
+        {
+            //Redirigir al SubMenú No Expropiativosnece
+            Form1 nuevoForm = new Form1();
+            this.Hide();
+            nuevoForm.Show();
+        }
+    }
+}
---
+++ AlgoritmosDePlanificacion/SubMenuNoExpropiativos.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AlgoritmosDePlanificacion
+{
+    public partial class SubMenuNoExpropiativos : Form
+    {
+        public SubMenuNoExpropiativos()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRegresarMenu_Click(object sender, EventArgs e)
+        {
+            //Redirigir al SubMenú No Expropiativosnece
+            Form1 nuevoForm = new Form1();
+            this.Hide();
+            nuevoForm.Show();
+        }
+
+        private void btnFIFO_Click(object sender, EventArgs e)
+        {
+            //Redirigir al FORM FIFO
+            FIFO nuevoForm = new FIFO();
+            this.Hide();
+            nuevoForm.Show();
+        }
+    }
+}
